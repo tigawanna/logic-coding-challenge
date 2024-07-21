a71eb23e78545851f853caeefb6566749e9734e0
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-07-21 18:57:01 +0200

    Add route protection

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Login from './components/auth/Login';
 import UserDetails from './components/pages/UserDetails';
+import PrivateRoute from './components/auth/PrivateRoute';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/user-details" element={<UserDetails />} />
+      <Route path="/user-details" element={<PrivateRoute />}>
+        <Route path="" element={<UserDetails />} />
+      </Route>
     </Routes>
   );
 }
