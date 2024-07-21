ae7f7a939eaead535dc322dbfe4c8d58f8b1c666
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-07-21 14:12:30 +0200

    Modify the server for improved error handlin

--- server/server.ts
+++ server/server.ts
@@ -17,39 +17,42 @@ app.get('/', (c) => {
 });
 
 app.post('/login', async (c) => {
-  const { username, password } = await c.req.json();
-  console.log(
-    ' ============= hono js ====================== ',
-    username,
-    password
-  );
-  if (username && username === '') {
-    return c.json({ message: 'username required' });
-  }
-  if (password && password === '') {
-    return c.json({ message: 'password required' });
-  }
-  const res = await fetch(
-    ' https://7qscqm2xvu2.us-west-2.awsapprunner.com/v1/auth/login',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'x-tenantid': 'SchryverPruebas',
-      },
-      body: JSON.stringify({
-        username,
-        password,
-      }),
-    }
-  );
-  if (!res.ok) {
-    return c.json({ message: `Auth Error: ${res.status}` });
-  }
+  try {
+    const { username, password } = await c.req.json();
 
-  const data = await res.json();
-  c.res.headers.append('Content-Type', 'application/json');
-  return c.json({ message: 'login success', data }, 200);
+    if (!username || username.trim() === '') {
+      return c.json({ message: 'Username required' }, 400);
+    }
+    if (!password || password.trim() === '') {
+      return c.json({ message: 'Password required' }, 400);
+    }
+
+    const res = await fetch(
+      'https://7qscqm2xvu2.us-west-2.awsapprunner.com/v1/auth/login',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-tenantid': 'SchryverPruebas',
+        },
+        body: JSON.stringify({
+          username,
+          password,
+        }),
+      }
+    );
+
+    if (!res.ok) {
+      return c.json({ message: `Auth Error: ${res.status}` });
+    }
+
+    const data = await res.json();
+    c.res.headers.append('Content-Type', 'application/json');
+    return c.json({ message: 'Login success', data }, 200);
+  } catch (error) {
+    console.error('Error in /login route:', error);
+    return c.json({ message: 'Internal Server Error' }, 500);
+  }
 });
 
 export default app;
