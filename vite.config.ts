1bac42ffdf0829e2fb33badea7f82fb7d06c5713
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-07-22 06:46:48 +0200

    Setup the vite config for production

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: process.env.VITE_PROXY_TARGET || 'http://localhost:3000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
